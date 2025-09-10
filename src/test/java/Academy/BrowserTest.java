217517d495f5bbc6b2054743574d7e10a33d5eda
vaessen31272 <marco.vaessen@gmail.com>
2025-09-10 18:38:15 +0200

    Update BrowserTest.java

--- src/test/java/Academy/BrowserTest.java
+++ src/test/java/Academy/BrowserTest.java
@@ -7,13 +7,18 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+
 
 
 
 public class BrowserTest {
 
 	
-	@Test
+	/* @Test
 	public void getData()
 	{
 		System.out.println("Hello Guys");
@@ -27,5 +32,22 @@ public class BrowserTest {
 	
 		
 		
-	}
+	} */
+
+	@Test
+    public void getData() {
+        System.out.println("Hello Guys");
+
+        // Automatically downloads and sets up the correct ChromeDriver version
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://azuredemo-ecg7bnh2ddbnbggf.westeurope-01.azurewebsites.net/webapp/");
+        String text = driver.findElement(By.cssSelector("h1")).getText();
+        System.out.println(text);
+
+        Assert.assertTrue(text.equalsIgnoreCase("RahulShettyAcademy.com Learning"));
+        driver.quit(); // Better cleanup than close()
+    }
+
 }
